d573ecc7db96c8a6c5e161e5b2fca0f633e29213
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-06-26 10:25:58 +0300

    add extended positive tests for searchPlace (excludes)

--- src/test/java/eDiscovery/tests/apiTests/extendedPositiveTestsApi/dealService/SearchPlaceExtendedPositiveTests.java
+++ src/test/java/eDiscovery/tests/apiTests/extendedPositiveTestsApi/dealService/SearchPlaceExtendedPositiveTests.java
@@ -1,19 +1,26 @@
 package eDiscovery.tests.apiTests.extendedPositiveTestsApi.dealService;
 import eDiscovery.TestBase;
+import eDiscovery.apiMethods.deal.ApiMethodsDealManipulation;
 import eDiscovery.apiMethods.deal.ApiMethodsSearchPlace;
+import eDiscovery.data.DataGeneratorDealService;
 import eDiscovery.helpers.enums.SearchPlaceCategoryType;
 import eDiscovery.helpers.enums.SearchPlaceType;
 import eDiscovery.models.deal.searchPlace.AddSearchPlaceRequestModel;
 import eDiscovery.models.deal.searchPlace.CommonSearchPlaceResponseModel;
 import eDiscovery.models.deal.searchPlace.SearchPlaceParametersModel;
+import eDiscovery.models.deal.searchPlace.UpdateSearchPlaceRequestModel;
+import eDiscovery.models.deal.searchQuery.CommonSearchQueryResponseModel;
 import eDiscovery.spec.RequestSpecifications;
 import eDiscovery.spec.ResponseSpecifications;
 import eDiscovery.spec.SpecificationsServer;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collections;
 import java.util.List;
 
 import static eDiscovery.data.DataGeneratorDealService.getRandomName;
@@ -216,5 +223,311 @@ public class SearchPlaceExtendedPositiveTests extends TestBase {
         assertThat(responseBody.excludes).isEqualTo(excludes);
     }
 
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение наименования места поиска с типом FileShare - SMB")
+    @Description("Тест проверяет возможность изменения наименования места поиска с типом FileShare - SMB")
+    @ParameterizedTest
+    @MethodSource("eDiscovery.data.DataGeneratorDealService#getValidSearchPlaceNames")
+    public void testUpdateSearchPlaceNameWithDifferentValidNames(String name){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = DataGeneratorDealService.createBasicSearchPlaceFileShareSMB();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .name(name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(responseBodySearchPlaceCreation.parameters)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .id(responseBodySearchPlaceCreation.id)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.name).isEqualTo(name);
+    }
+
+    @Test
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение наименования места поиска, которое используется в созданном деле")
+    @Description("Тест проверяет возможность изменения наименования места поиска, которое используется в созданном деле")
+    public void testUpdateSearchPlaceNameUsedInCreatedDeal(){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = DataGeneratorDealService.createBasicSearchPlaceFileShareSMB();
+        CommonSearchQueryResponseModel responseBodySearchQueryCreation = DataGeneratorDealService.createSearchQueryWithOnlyRequiredParameters();
+
+        ApiMethodsDealManipulation.addDeal(DataGeneratorDealService.getBasicDealManipulationModel(
+                Collections.singletonList(responseBodySearchPlaceCreation.id),
+                Collections.singletonList(responseBodySearchQueryCreation.id)
+        ));
+
+        String newName = DataGeneratorDealService.getRandomName();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .name(newName)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(responseBodySearchPlaceCreation.parameters)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .id(responseBodySearchPlaceCreation.id)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.name).isEqualTo(newName);
+    }
+
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение categoryType в месте поиска с categoryType = FileShare, если место поиска не используется в деле")
+    @Description("Тест проверяет возможность изменения categoryType в месте поиска с categoryType = FileShare, если место поиска не используется в деле")
+    @ParameterizedTest
+    @EnumSource(SearchPlaceCategoryType.class)
+    public void testUpdateSearchPlaceCategoryTypeInFileShareWithoutDeal(SearchPlaceCategoryType categoryType){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = DataGeneratorDealService.createBasicSearchPlaceFileShareSMB();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(responseBodySearchPlaceCreation.parameters)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .id(responseBodySearchPlaceCreation.id)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.categoryType).isEqualTo(categoryType);
+    }
+
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение type в месте поиска с categoryType = FileShare, если место поиска не используется в деле")
+    @Description("Тест проверяет возможность изменения type в месте поиска с categoryType = FileShare, если место поиска не используется в деле")
+    @ParameterizedTest
+    @EnumSource(SearchPlaceType.class)
+    public void testUpdateSearchPlaceTypeInFileShareWithoutDeal(SearchPlaceType type){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = DataGeneratorDealService.createBasicSearchPlaceFileShareSMB();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(type)
+                .parameters(responseBodySearchPlaceCreation.parameters)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .id(responseBodySearchPlaceCreation.id)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.type).isEqualTo(type);
+    }
+
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение uri в параметрах места поиска с типом FileShare - SMB")
+    @Description("Тест проверяет возможность изменения uri в параметрах места поиска с типом FileShare - SMB")
+    @ParameterizedTest
+    @MethodSource("eDiscovery.data.DataGeneratorDealService#getValidSearchPlaceURIInParameters")
+    public void testUpdateSearchPlaceUriParametersWithDifferentValidValues(String uri){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        SearchPlaceParametersModel initialParameters = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username("userName")
+                .password("p@$$w0rd")
+                .build();
+
+        AddSearchPlaceRequestModel requestBodySearchPlaceCreation = AddSearchPlaceRequestModel.builder()
+                .name(DataGeneratorDealService.getRandomName())
+                .parameters(initialParameters)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = ApiMethodsSearchPlace.addSearchPlace(requestBodySearchPlaceCreation).as(CommonSearchPlaceResponseModel.class);
+
+        SearchPlaceParametersModel parametersForUpdate = SearchPlaceParametersModel.builder()
+                .uri(uri)
+                .username("userName")
+                .password("p@$$w0rd")
+                .build();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .id(responseBodySearchPlaceCreation.id)
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(parametersForUpdate)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .build();
+
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.parameters).isEqualTo(parametersForUpdate);
+    }
+
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение username в параметрах места поиска с типом FileShare - SMB")
+    @Description("Тест проверяет возможность изменения username в параметрах места поиска с типом FileShare - SMB")
+    @ParameterizedTest
+    @MethodSource("eDiscovery.data.DataGeneratorDealService#getValidSearchPlaceUsernamesInParameters")
+    public void testUpdateSearchPlaceUsernameParametersWithDifferentValidValues(String username){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        SearchPlaceParametersModel initialParameters = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username("userName")
+                .password("p@$$w0rd")
+                .build();
+
+        AddSearchPlaceRequestModel requestBodySearchPlaceCreation = AddSearchPlaceRequestModel.builder()
+                .name(DataGeneratorDealService.getRandomName())
+                .parameters(initialParameters)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = ApiMethodsSearchPlace.addSearchPlace(requestBodySearchPlaceCreation).as(CommonSearchPlaceResponseModel.class);
+
+        SearchPlaceParametersModel parametersForUpdate = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username(username)
+                .password("p@$$w0rd")
+                .build();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .id(responseBodySearchPlaceCreation.id)
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(parametersForUpdate)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .build();
+
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.parameters).isEqualTo(parametersForUpdate);
+    }
+
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение password в параметрах места поиска с типом FileShare - SMB")
+    @Description("Тест проверяет возможность изменения password в параметрах места поиска с типом FileShare - SMB")
+    @ParameterizedTest
+    @MethodSource("eDiscovery.data.DataGeneratorDealService#getValidSearchPlacePasswordsInParameters")
+    public void testUpdateSearchPlacePasswordParametersWithDifferentValidValues(String password){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        SearchPlaceParametersModel initialParameters = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username("userName")
+                .password("p@$$w0rd")
+                .build();
+
+        AddSearchPlaceRequestModel requestBodySearchPlaceCreation = AddSearchPlaceRequestModel.builder()
+                .name(DataGeneratorDealService.getRandomName())
+                .parameters(initialParameters)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = ApiMethodsSearchPlace.addSearchPlace(requestBodySearchPlaceCreation).as(CommonSearchPlaceResponseModel.class);
+
+        SearchPlaceParametersModel parametersForUpdate = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username("userName")
+                .password(password)
+                .build();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .id(responseBodySearchPlaceCreation.id)
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(parametersForUpdate)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .build();
+
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.parameters).isEqualTo(parametersForUpdate);
+    }
+
+    @Test
+    @Epic("Сервис Deal")
+    @Feature("Место поиска")
+    @Story("Изменение места поиска")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Изменение параметров места поиска, которое используется в созданном деле")
+    @Description("Тест проверяет возможность изменения параметров места поиска, которое используется в созданном деле")
+    public void testUpdateSearchPlaceParametersUsedInCreatedDeal(){
+        SpecificationsServer.installRequestSpecification(RequestSpecifications.basicRequestSpecificationWithAuthorization());
+        SpecificationsServer.installResponseSpecification(ResponseSpecifications.responseSpecOK200JSONBody());
+
+        SearchPlaceParametersModel initialParameters = SearchPlaceParametersModel.builder()
+                .uri("ur1")
+                .username("userName")
+                .password("p@$$w0rd")
+                .build();
+
+        AddSearchPlaceRequestModel requestBodySearchPlaceCreation = AddSearchPlaceRequestModel.builder()
+                .name(DataGeneratorDealService.getRandomName())
+                .parameters(initialParameters)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceCreation = ApiMethodsSearchPlace.addSearchPlace(requestBodySearchPlaceCreation).as(CommonSearchPlaceResponseModel.class);
+        CommonSearchQueryResponseModel responseBodySearchQueryCreation = DataGeneratorDealService.createSearchQueryWithOnlyRequiredParameters();
+
+        ApiMethodsDealManipulation.addDeal(DataGeneratorDealService.getBasicDealManipulationModel(
+                Collections.singletonList(responseBodySearchPlaceCreation.id),
+                Collections.singletonList(responseBodySearchQueryCreation.id)
+        ));
+
+        SearchPlaceParametersModel parametersForUpdate = SearchPlaceParametersModel.builder()
+                .uri("ur12")
+                .username("userName2")
+                .password("p@$$w0rd2")
+                .build();
+
+        UpdateSearchPlaceRequestModel requestSearchPlaceUpdate = UpdateSearchPlaceRequestModel.builder()
+                .id(responseBodySearchPlaceCreation.id)
+                .name(responseBodySearchPlaceCreation.name)
+                .categoryType(responseBodySearchPlaceCreation.categoryType)
+                .type(responseBodySearchPlaceCreation.type)
+                .parameters(parametersForUpdate)
+                .excludes(responseBodySearchPlaceCreation.excludes)
+                .build();
+
+        CommonSearchPlaceResponseModel responseBodySearchPlaceUpdate = ApiMethodsSearchPlace.updateSearchPlace(requestSearchPlaceUpdate).as(CommonSearchPlaceResponseModel.class);
+
+        assertThat(responseBodySearchPlaceUpdate.parameters).isEqualTo(parametersForUpdate);
+    }
 
 }
