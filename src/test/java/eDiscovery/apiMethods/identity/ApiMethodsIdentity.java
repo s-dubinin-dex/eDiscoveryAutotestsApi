44b769c60ce465c161a6f7fe6e6a7c4a44dfe207
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-11-05 15:02:45 +0200

    authorization scope

--- src/test/java/eDiscovery/apiMethods/identity/ApiMethodsIdentity.java
+++ src/test/java/eDiscovery/apiMethods/identity/ApiMethodsIdentity.java
@@ -19,7 +19,6 @@ public class ApiMethodsIdentity extends UrlBase {
                 .formParam("client_id", "admin.client")
                 .formParam("client_secret", "9F45EA47-9BD6-48D8-B218-273A256DB093")
                 .formParam("grant_type", "password")
-                .formParam("scope", "openid profile offline_access admin-api policy")
                 .formParam("username", "test@gmail.com")
                 .formParam("password", "005")
                 .when()
