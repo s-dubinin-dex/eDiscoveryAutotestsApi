20fc81596ae4f3e2cd6e6e9c688061262eeeac20
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-11-05 15:20:54 +0200

    data checker utc datetime format

--- src/test/java/eDiscovery/helpers/DataChecker.java
+++ src/test/java/eDiscovery/helpers/DataChecker.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class DataChecker {
 
     public static String dateTimeUTCPattern(){
-        return "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2,6}Z$";
+        return "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2,7}Z$";
     }
 
     public static String dateTimeISOPattern(){
