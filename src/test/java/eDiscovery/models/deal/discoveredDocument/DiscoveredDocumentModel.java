ff97f4997d54a87bf3266a06cdb928b51b840301
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-11-08 16:40:34 +0200

    upd DiscoveredDocumentModel

--- src/test/java/eDiscovery/models/deal/discoveredDocument/DiscoveredDocumentModel.java
+++ src/test/java/eDiscovery/models/deal/discoveredDocument/DiscoveredDocumentModel.java
@@ -4,6 +4,11 @@ public class DiscoveredDocumentModel {
     public String id;
     public String dealId;
     public String findedUtc;
-    public String fileName;
     public String filePath;
+    public String fileName;
+    public String fileType;
+    public String innerFilePath;
+    public String innerFileName;
+    public String searchProblemReason;
+    public String searchPlace;
 }
