3f91b399962fc8597032db7b8ba4f8274e954194
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-11-25 17:51:35 +0200

    add classifier_url to properties

--- src/test/java/eDiscovery/UrlBase.java
+++ src/test/java/eDiscovery/UrlBase.java
@@ -2,9 +2,10 @@ package eDiscovery;
 
 public class UrlBase {
     protected static String TOKEN;
-    protected static final String BASE_URL      = System.getProperty("BASE_URL");
-    protected static final String IDENTITY_URL  = System.getProperty("IDENTITY_URL");
-    protected static final String ADMIN_URL     = System.getProperty("ADMIN_URL");
-    protected static final String DEAL_URL      = System.getProperty("DEAL_URL");
+    protected static final String BASE_URL          = System.getProperty("BASE_URL");
+    protected static final String IDENTITY_URL      = System.getProperty("IDENTITY_URL");
+    protected static final String ADMIN_URL         = System.getProperty("ADMIN_URL");
+    protected static final String DEAL_URL          = System.getProperty("DEAL_URL");
+    protected static final String CLASSIFIER_URL    = System.getProperty("CLASSIFIER_URL");
 
 }
