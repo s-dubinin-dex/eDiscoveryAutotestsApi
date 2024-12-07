1d76c5cf5641e88c676151b76e4e839798072475
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-12-07 22:02:30 +0200

    Обновил модель для markerResult в Deal и Classifier сервисе

--- src/test/java/eDiscovery/models/classifier/markerResult/CommonMarkerResultResponseModel.java
+++ src/test/java/eDiscovery/models/classifier/markerResult/CommonMarkerResultResponseModel.java
@@ -1,7 +1,33 @@
 package eDiscovery.models.classifier.markerResult;
 
+import eDiscovery.models.classifier.marker.CommonMarkerResponseModel;
+import eDiscovery.models.classifier.profile.CommonProfileResponseModel;
+
 public class CommonMarkerResultResponseModel {
 
-
+    public String id;
+    public String externalId;
+    public String externalDetailId;
+    public String externalObjectId;
+    public String actionDate;
+    public String filePath;
+    public String fileName;
+    public String innerFilePath;
+    public String innerFileName;
+    public String processDescription;
+    public String errorReason;
+    public boolean isStartMarkerFinded;
+    public boolean isResultMarkerFinded;
+    public String externalStartMarkerId;
+    public String externalResultMarkerId;
+    public CommonMarkerResponseModel startMarker;
+    public CommonMarkerResponseModel resultMarker;
+    public CommonProfileResponseModel profileUsed;
+    public boolean isStartPolicyFinded;
+    public boolean isResultPolicyFinded;
+    public String externalStartPolicyId;
+    public String externalResultPolicyId;
+    public String startPolicy;
+    public String resultPolicy;
 
 }
