ac8977140d5381998e3663953591c664d140d788
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-12-27 17:05:41 +0200

    обновил модель MatchResult

--- src/test/java/eDiscovery/models/deal/matchResult/MatchResultResponseModel.java
+++ src/test/java/eDiscovery/models/deal/matchResult/MatchResultResponseModel.java
@@ -3,10 +3,6 @@ package eDiscovery.models.deal.matchResult;
 public class MatchResultResponseModel {
     public String id;
     public String discoveredDocumentId;
-    public String filePath;
-    public String fileName;
-    public String innerFilePath;
-    public String innerFileName;
     public String searchQueryId;
     public String searchQueryName;
     public int count;
