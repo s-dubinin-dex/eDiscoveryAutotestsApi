2f94c39e60b36005242b0fdde38dabd4765b8f0a
Sergey Dubinin <s.dubinin@dex-it.ru>
2024-12-27 16:04:10 +0200

    обновил модель ответа Agents

--- src/test/java/eDiscovery/models/deal/agents/CommonAgentsResponseModel.java
+++ src/test/java/eDiscovery/models/deal/agents/CommonAgentsResponseModel.java
@@ -4,14 +4,14 @@ import eDiscovery.models.deal.agentActivity.CommonAgentActivityResponseModel;
 import eDiscovery.models.odata.OdataEntityResponseModel;
 
 public class CommonAgentsResponseModel extends OdataEntityResponseModel {
-
     public String id;
     public String userName;
     public String machineName;
     public String agentType;
     public String osVersion;
-    public String agentVersion;
-    public String searchPlaceId;
+    public int agentVersion;
+    public String agentVersionString;
+    public SearchPlaceAgentModel searchPlace;
     public CommonAgentActivityResponseModel lastActivity;
-
+    public String createdUtc;
 }
